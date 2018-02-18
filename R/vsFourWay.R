fb344b18f87ff0bb0c312a45adba7346802afd30
btmonier <brandon.monier@gmail.com>
2018-02-18 16:26:20 -0600

    Remove 'shape' column in data return
    Former-commit-id: b8c02aa8c08e4ea09ef7e811b5a11b720acc674c
    Former-commit-id: 57b5c6d35b0b629257ab4fd58978cfac9ff77459 [formerly 72b680e94c3c5f4b39a2a65252183812501fe9bd]
    Former-commit-id: 0bb1009675db0a34f509c010c6654e0f5a0d94ba

--- R/vsFourWay.R
+++ R/vsFourWay.R
@@ -172,7 +172,8 @@ vsFourWay <- function(
         leg
     
     if (isTRUE(data.return)) {
-        plot.l <- list(data = dat, plot = tmp.plot)
+        dat2 <- dat[, -ncol(dat)]
+        plot.l <- list(data = dat2, plot = tmp.plot)
     } else {
         print(tmp.plot)
     }
