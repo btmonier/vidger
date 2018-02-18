4069f55b9f0b60e495065f836d72acb5ef92d797
btmonier <brandon.monier@gmail.com>
2018-02-18 16:26:20 -0600

    Remove 'shape' column in data return
    Former-commit-id: b8c02aa8c08e4ea09ef7e811b5a11b720acc674c
    Former-commit-id: fb344b18f87ff0bb0c312a45adba7346802afd30 [formerly 0bb1009675db0a34f509c010c6654e0f5a0d94ba] [formerly fb344b18f87ff0bb0c312a45adba7346802afd30 [formerly 0bb1009675db0a34f509c010c6654e0f5a0d94ba] [formerly 57b5c6d35b0b629257ab4fd58978cfac9ff77459 [formerly 72b680e94c3c5f4b39a2a65252183812501fe9bd]]]
    Former-commit-id: 606b103b93aee38c3e8fc295adef379a8edf0be7
    Former-commit-id: 62751f8518b795047de8b7bedd4733ad2240d43e [formerly 6baa0f1b9e88846fae8ad255673ae32f3617359c]
    Former-commit-id: 07eafbdf481336e840188afaf8036a148ee358ab

--- R/vsMAPlot.R
+++ R/vsMAPlot.R
@@ -136,7 +136,8 @@ vsMAPlot <- function(
         comp2$size + grid + leg
     
     if (isTRUE(data.return)) {
-        plot.l <- list(data = dat, plot = tmp.plot)
+        dat2 <- dat[, -ncol(dat)]
+        plot.l <- list(data = dat2, plot = tmp.plot)
     } else {
         print(tmp.plot)
     }
