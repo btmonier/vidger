34db8c32a2e2fd676a07d28d697274ff134df011
btmonier <brandon.monier@gmail.com>
2018-02-22 14:14:51 -0600

    Code formatting
    Former-commit-id: 70bf1abff49b93a4cbbc6d28b8ca8bdca1925293
    Former-commit-id: 415a202e2442235e8512371fb7cd375a219343e3 [formerly f1b4704c2486df74c86ce10751f4d0f2b08c94a6] [formerly 415a202e2442235e8512371fb7cd375a219343e3 [formerly f1b4704c2486df74c86ce10751f4d0f2b08c94a6] [formerly 415a202e2442235e8512371fb7cd375a219343e3 [formerly f1b4704c2486df74c86ce10751f4d0f2b08c94a6] [formerly bf41aa22fe1da5360a0c4d415de55216ec793624 [formerly 1fa79fe6eaa72a2df6181ab9234902345025ea42]]]]
    Former-commit-id: 47b27ed4768a004fc570cb49e5d433093feb9b14
    Former-commit-id: e17327d447b5cae4f4f78fabcc930b2df4101139 [formerly 9753a928bf9dc712107aa3acca318b339343890f] [formerly 73c49e20670e3244522a172c38bc26605d6ceeac [formerly 0dcc716668e833c9de316c2adde7b17ab84ed2a4]]
    Former-commit-id: 69e8cf80c2152a04654fb8ccb713446920eabcf2
    Former-commit-id: 2c9fa033b35f8203bf662cf6da196f002e1f397e

--- tests/testthat/test-volcano-plots.R
+++ tests/testthat/test-volcano-plots.R
@@ -2,7 +2,7 @@ context("volcano plots")
 
 test_that("volcano plots work", {
     # Load data
-    data(df.cuffdiff)
+    data(df.cuff)
     data(df.deseq)
     data(df.edger)
 
@@ -12,10 +12,10 @@ test_that("volcano plots work", {
               title = TRUE, legend = TRUE, grid = TRUE, data.return = FALSE)
 
     # DESeq2
-    vsVolcano(x = 'treated', y = 'untreated', data = df.deseq, 
-              d.factor = 'condition', type = 'deseq', padj = 0.05, 
-              x.lim = NULL, lfc = NULL, title = TRUE, legend = TRUE, 
-              grid = TRUE, data.return = FALSE)
+    vsVolcano(x = 'treated_paired.end', y = 'untreated_paired.end', 
+              data = df.deseq, d.factor = 'condition', type = 'deseq', 
+              padj = 0.05, x.lim = NULL, lfc = NULL, title = TRUE, 
+              legend = TRUE, grid = TRUE, data.return = FALSE)
 
     # edgeR
     vsVolcano(x = 'WW', y = 'MM', data = df.edger, d.factor = NULL, 
