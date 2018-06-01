f338fe8fd07652876684134ba49d3f6008d04a08
btmonier <brandon.monier@gmail.com>
2018-05-31 20:32:31 -0500

    Add ID return

--- R/vsScatterPlot-house-keeping.R
+++ R/vsScatterPlot-house-keeping.R
@@ -10,7 +10,15 @@
     tmp.y <- row.names(data$samples[which(data$samples$group == y), ])
     x <- rowMeans(dat.cpm[, tmp.x])
     y <- rowMeans(dat.cpm[, tmp.y])
-    dat.plt <- data.frame(x, y)
+
+    if (is.null(data$genes)) {
+        pad <- paste0("%0", nchar(nrow(data$counts)), "d")
+        id <- paste0("ID_", sprintf(pad, seq(nrow(data$counts))))
+    } else {
+        id <- data$genes
+    }
+
+    dat.plt <- data.frame(id, x, y, stringsAsFactors = FALSE)
     return(dat.plt)
 }
 
@@ -21,7 +29,7 @@
     deg <- data
     deg <- subset(deg, (sample_1 == x & sample_2 == y) | 
                                     (sample_1 == y & sample_2 == x))
-    dat <- data.frame(test_id = deg$test_id)
+    dat <- data.frame(id = deg$test_id, stringsAsFactors = FALSE)
     
     if (x %in% deg$sample_1 && y %in% deg$sample_2) {
         dat$x <- deg$value_1
@@ -48,6 +56,7 @@
     nam_y <- row.names(dat1[which(dat1[d.factor] == y),])
     x <- rowMeans(dat2[, nam_x])
     y <- rowMeans(dat2[, nam_y])
-    dat3 <- data.frame(x, y)
+    id <- rownames(dat2)
+    dat3 <- data.frame(id, x, y, stringsAsFactors = FALSE)
     return(dat3)
 }
